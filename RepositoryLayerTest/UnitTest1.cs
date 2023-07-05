9342a790918041d01356530499aaf0438cd5d172
SyuanTsai <Syuan.Tsai@dash.com.au>
2023-07-05 18:56:39 +1000

    Sln Init

---
+++ RepositoryLayerTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace RepositoryLayerTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
